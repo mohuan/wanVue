230b8e2fa169ba4f1a377c32ad94f0543d021a36
weidingqiang <weidingqiang@163.com>
2019-10-30 14:31:34 +0800

    新增axios支持

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,3 +1,7 @@
+/**
+ * 可以进行异步操作
+ * 调用方法为dispatch
+ */
 export default {
   spinshow(context,flg){
     context.commit('spinshow',flg)
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,3 +1,6 @@
+/**
+ * mutations 为同步方法
+ */
 export default {
   spinshow(state,flg){
     state.spinShow = flg
